b1135ccef267c758ecf98b59d5cdd6a636b6689b
Tim Han <seungt.han@gmail.com>
2019-04-03 15:31:30 -0400

    Added a profile picture and animation

--- src/ui/components/Welcome.js
+++ src/ui/components/Welcome.js
@@ -4,14 +4,19 @@ export default class Welcome extends Component {
   render() {
     return (
       <div id="welcome-container">
-        <div>
-          <h1 className="first-line">Hi, my name is</h1>
-          <h1 className="second-line">Tim Han</h1>
-          <h1 className="third-line">I'm a software engineer</h1>
+        <div className="first-paragraph-container">
+          <div>
+            <h1 className="first-line">Hi, my name is</h1>
+            <h1 className="second-line">Tim Han</h1>
+            <h1 className="third-line">Software Engineer</h1>
+          </div>
+          <div>
+            <img className="profile-pic" src="profile.png"/>
+          </div>
         </div>
         <div>
           <p className='fourth-line'>
-            {'\I\'m a software engineer based in NYC, NY specializing in building web (both front-end and back-end) and data-related applications. I currently work at '}
+            {'\I\'m a software engineer based in NYC, NY specializing in building web (front-end and back-end) and data-related applications. I currently work at '}
             <span><a href='https://unified.com/' target='_blank' style={{color: '#45ce95', textDecoration: 'none'}}>{'Unified'}</a></span>
             {' as a Software Engineer.'}
           </p>
