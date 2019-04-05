972bc94ac38df58a1623d81ddf016b673449ed21
yeb9925 <seungt.han@gmail.com>
2019-04-05 00:34:30 -0400

    Added contact page

--- src/ui/components/Contact.js
+++ src/ui/components/Contact.js
@@ -4,24 +4,19 @@ export default class Contact extends Component {
   render() {
     return (
       <div id="contact-container">
-        <form id="contact-form">
-          <label className="contact-input">
-            Name:<br/>
-            <input type="text" name="name"/>
-          </label>
-          <label className="contact-input">
-            Email:<br/>
-            <input type="text" name="email"/>
-          </label>
-          <label className="contact-input">
-            Subject:<br/>
-            <input type="text" name="subject"/>
-          </label>
-          <label className="contact-input">
-            Description:<br/>
-            <input type="text" name="description"/>
-          </label>
-        </form>
+        <div className="contact">
+          <h1 className="contact-header">Get In Touch!<span></span></h1>
+        </div>
+        <div className="contact-description">
+          <p className="contact-paragraph">
+            {'I\'\m open to any freelance work like making websites like this. Feel free to reach me out on any platform for any questions, advice, and/or talk. My inbox is always opened!'}
+          </p>
+        </div>
+        <div className="contact-email-container">
+          <div className="contact-email">
+            <h1 className="scontact-me"><a href="mailto:seungt.han@gmail.com" style={{textDecoration: 'none', color: '#45ce95'}}>Contact</a></h1>
+          </div>
+        </div>
       </div>
     )
   }
