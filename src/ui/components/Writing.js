42c11da6ec5c83639b42b80fbeb41a5b507ad7ab
Tim Han <seungt.han@gmail.com>
2019-04-08 18:39:09 -0400

    Mobile writing page

--- src/ui/components/Writing.js
+++ src/ui/components/Writing.js
@@ -29,18 +29,37 @@ export default class Writing extends Component {
             <p className="writing-text">Sorting algorithms are very important to know and implement. Learn about merge sort and how to implement it using JavaScript!</p>
           </div>
         </div>
-        <div className="writing-one">
-          <div className="writing-description-two">
-            <h3 className="writing-publication"><span><a href="https://hackernoon.com/" target="_blank" style={{color: '#45ce95', textDecoration: 'none'}}>Hackernoon</a></span></h3>
-            <h2 className="writing-title"><span><a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank" style={{color: '#e6f1fe', textDecoration: 'none'}}>3 Programming Languages to Learn in 2019</a></span></h2>
-            <p className="writing-text">Here are my list of 3 languages you should pick up in 2019!</p>
-          </div>
-          <div className="writing-image-container">
-            <a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank">
-              <img className="writing-image" src="/writing-images/writing2.png" />
-            </a>
-          </div>
-        </div>
+        {
+          window.innerWidth < 1200
+            ? (
+              <div className="writing-one" style={{marginBottom: '60px'}}>
+                <div className="writing-image-container">
+                  <a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank">
+                    <img className="writing-image" src="/writing-images/writing2.png" />
+                  </a>
+                </div>
+                <div className="writing-description-two">
+                  <h3 className="writing-publication"><span><a href="https://hackernoon.com/" target="_blank" style={{color: '#45ce95', textDecoration: 'none'}}>Hackernoon</a></span></h3>
+                  <h2 className="writing-title"><span><a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank" style={{color: '#e6f1fe', textDecoration: 'none'}}>3 Programming Languages to Learn in 2019</a></span></h2>
+                  <p className="writing-text">Here are my list of 3 languages you should pick up in 2019!</p>
+                </div>
+              </div>
+            )
+            : (
+              <div className="writing-one">
+                <div className="writing-description-two">
+                  <h3 className="writing-publication"><span><a href="https://hackernoon.com/" target="_blank" style={{color: '#45ce95', textDecoration: 'none'}}>Hackernoon</a></span></h3>
+                  <h2 className="writing-title"><span><a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank" style={{color: '#e6f1fe', textDecoration: 'none'}}>3 Programming Languages to Learn in 2019</a></span></h2>
+                  <p className="writing-text">Here are my list of 3 languages you should pick up in 2019!</p>
+                </div>
+                <div className="writing-image-container">
+                  <a href="https://hackernoon.com/3-programming-languages-to-learn-in-2019-2eb035936f65" target="_blank">
+                    <img className="writing-image" src="/writing-images/writing2.png" />
+                  </a>
+                </div>
+              </div>
+            )
+        }
         <div className="more-writing-container">
           <div className="more-writing">
             <h1 className="more-writing-text"><a href="https://medium.com/@timhancodes" target="_blank" style={{color: '#0e162f', textDecoration: 'none'}}>More</a></h1>
